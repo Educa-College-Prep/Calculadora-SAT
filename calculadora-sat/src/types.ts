e4285e11f6687e4d1a69da4665a7f25c8dbdf960
rcarbajalc <rcarbajal50@outlook.com>
2026-07-31 14:20:40 -0500

    old commit

--- calculadora-sat/src/types.ts
+++ calculadora-sat/src/types.ts
@@ -21,38 +21,17 @@ export interface Universidad {
   CIPTITLE5?: string | null;
   CIPTITLE6?: string | null;
   ICLEVEL?: string | null;
-
-  // Costos
   TUITIONFEE_OUT?: number | null;
-  TUITIONFEE_IN?: number | null;
   COSTT4_A?: number | null;
-
-  // Precio neto promedio general
   NPT4_PRIV?: number | null;
   NPT4_PUB?: number | null;
-
-  // Precio neto por rango de ingreso familiar (1=bajo, 3=medio, 5=alto)
-  NPT41_PRIV?: number | null;
-  NPT41_PUB?: number | null;
-  NPT43_PRIV?: number | null;
-  NPT43_PUB?: number | null;
-  NPT45_PRIV?: number | null;
-  NPT45_PUB?: number | null;
-
-  // Ratio estudiante-facultad
-  STUFACR?: number | null;
-
-  // Salarios de egresados
+  C150_4?: number | null;
+  C150_L4?: number | null;
+  C150_4_HISP?: number | null;
+  C150_L4_HISP?: number | null;
   MD_EARN_WNE_P6?: number | null;
   MD_EARN_WNE_P8?: number | null;
   MD_EARN_WNE_P10?: number | null;
   MD_EARN_WNE_1YR?: number | null;
   MD_EARN_WNE_5YR?: number | null;
-
-  // % que supera salario de secundaria
-  GT_THRESHOLD_P6?: number | null;
-  GT_THRESHOLD_P8?: number | null;
-  GT_THRESHOLD_P10?: number | null;
-  GT_THRESHOLD_1YR?: number | null;
-  GT_THRESHOLD_5YR?: number | null;
 }
